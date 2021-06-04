d7cb4e53d5ceecb78398eac1a0b6fa424c50603c
Benjamin Rosseaux <benjamin@rosseaux.de>
2021-06-05 01:12:06 +0200

    Microoptimizations

--- src/PasDblStrUtils.pas
+++ src/PasDblStrUtils.pas
@@ -1,7 +1,7 @@
 (******************************************************************************
  *                               PasDblStrUtils                               *
  ******************************************************************************
- *                        Version 2021-06-04-00-45-0000                       *
+ *                        Version 2021-06-04-01-11-0000                       *
  ******************************************************************************
  *                                zlib license                                *
  *============================================================================*
@@ -3381,88 +3381,116 @@ var StringPosition:TPasDblStrUtilsInt32;
     HasDigits,Negative,ExponentNegative:boolean;
     c:TPasDblStrUtilsChar;
 begin
- if assigned(aOK) then begin
-  aOK^:=false;
- end;
- HasDigits:=false;
+
  Negative:=false;
  StringPosition:=0;
  Base10Mantissa:=0;
- Base10Exponent:=1;
+ Base10Exponent:=0;
+
  while (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['-','+']) do begin
   Negative:=Negative xor (aStringValue[StringPosition]='-');
   inc(StringPosition);
  end;
- while (StringPosition<aStringLength) and (aStringValue[StringPosition]='0') do begin
-  HasDigits:=true;
-  inc(StringPosition);
- end;
- while StringPosition<aStringLength do begin
-  c:=aStringValue[StringPosition];
-  case c of
-   '0'..'9':begin
-    HasDigits:=true;
-    Base10Mantissa:=(Base10Mantissa*10)+TPasDblStrUtilsUInt64(TPasDblStrUtilsUInt8(TPasDblStrUtilsChar(aStringValue[StringPosition]))-TPasDblStrUtilsUInt8(TPasDblStrUtilsChar('0')));
-    if (Base10Mantissa and InvBit53Mask)<>0 then begin
-     HasDigits:=false;
-     break;
-    end;
-    if Base10Exponent<=0 then begin
-     dec(Base10Exponent);
-    end;
-   end;
-   '.':begin
-    if Base10Exponent<=0 then begin
-     HasDigits:=false;
-     break;
-    end else begin
-     Base10Exponent:=0;
-    end;
-   end;
-   'e','E':begin
-    break;
-   end;
-   else begin
-    HasDigits:=false;
-    break;
-   end;
-  end;
-  inc(StringPosition);
- end;
+
+ HasDigits:=(StringPosition<aStringLength) and (aStringValue[StringPosition] in ['0'..'9']);
  if HasDigits then begin
-  if Base10Exponent>0 then begin
-   Base10Exponent:=0;
+  while StringPosition<aStringLength do begin
+   c:=aStringValue[StringPosition];
+   case c of
+    '0'..'9':begin
+     Base10Mantissa:=(Base10Mantissa*10)+TPasDblStrUtilsUInt64(TPasDblStrUtilsUInt8(TPasDblStrUtilsChar(aStringValue[StringPosition]))-TPasDblStrUtilsUInt8(TPasDblStrUtilsChar('0')));
+     if (Base10Mantissa and InvBit53Mask)<>0 then begin
+      result:=UInt64Bits2Double(TPasDblStrUtilsUInt64($7ff8000000000000)); // NaN
+      if assigned(aOK) then begin
+       aOK^:=false;
+      end;
+      exit;
+     end;
+     inc(StringPosition);
+    end;
+    else begin
+     break;
+    end;
+   end;
   end;
-  ExponentValue:=0;
-  ExponentNegative:=false;
-  if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['e','E']) then begin
+ end;
+
+ if (StringPosition<aStringLength) and (aStringValue[StringPosition]='.') then begin
+  inc(StringPosition);
+  if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['0'..'9']) then begin
+   HasDigits:=true;
+   while StringPosition<aStringLength do begin
+    c:=aStringValue[StringPosition];
+    case c of
+     '0'..'9':begin
+      Base10Mantissa:=(Base10Mantissa*10)+TPasDblStrUtilsUInt64(TPasDblStrUtilsUInt8(TPasDblStrUtilsChar(aStringValue[StringPosition]))-TPasDblStrUtilsUInt8(TPasDblStrUtilsChar('0')));
+      if (Base10Mantissa and InvBit53Mask)<>0 then begin
+       result:=UInt64Bits2Double(TPasDblStrUtilsUInt64($7ff8000000000000)); // NaN
+       if assigned(aOK) then begin
+        aOK^:=false;
+       end;
+       exit;
+      end;
+      inc(StringPosition);
+      dec(Base10Exponent);
+     end;
+     else begin
+      break;
+     end;
+    end;
+   end;
+  end;
+ end;
+
+ if not HasDigits then begin
+  result:=UInt64Bits2Double(TPasDblStrUtilsUInt64($7ff8000000000000)); // NaN
+  if assigned(aOK) then begin
+   aOK^:=false;
+  end;
+  exit;
+ end;
+
+ if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['e','E']) then begin
+  inc(StringPosition);
+  if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['+','-']) then begin
+   ExponentNegative:=aStringValue[StringPosition]='-';
    inc(StringPosition);
-   if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['+','-']) then begin
-    ExponentNegative:=aStringValue[StringPosition]='-';
-    inc(StringPosition);
-   end;
-   HasDigits:=false;
-   while (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['0'..'9']) do begin
-    ExponentValue:=(ExponentValue*10)+TPasDblStrUtilsInt32(TPasDblStrUtilsUInt8(TPasDblStrUtilsChar(aStringValue[StringPosition]))-TPasDblStrUtilsUInt8(TPasDblStrUtilsChar('0')));
-    HasDigits:=true;
-    inc(StringPosition);
-   end;
-  end;
-  if Base10Mantissa=0 then begin
-   Base10Exponent:=0;
   end else begin
+   ExponentNegative:=false;
+  end;
+  if (StringPosition<aStringLength) and (aStringValue[StringPosition] in ['0'..'9']) then begin
+   ExponentValue:=0;
+   repeat
+    ExponentValue:=(ExponentValue*10)+TPasDblStrUtilsInt32(TPasDblStrUtilsUInt8(TPasDblStrUtilsChar(aStringValue[StringPosition]))-TPasDblStrUtilsUInt8(TPasDblStrUtilsChar('0')));
+    inc(StringPosition);
+   until (StringPosition>=aStringLength) or not (aStringValue[StringPosition] in ['0'..'9']);
    if ExponentNegative then begin
     dec(Base10Exponent,ExponentValue);
    end else begin
     inc(Base10Exponent,ExponentValue);
    end;
+  end else begin
+   result:=UInt64Bits2Double(TPasDblStrUtilsUInt64($7ff8000000000000)); // NaN
+   if assigned(aOK) then begin
+    aOK^:=false;
+   end;
+   exit;
   end;
  end;
- if HasDigits and (StringPosition>=aStringLength) then begin
+
+ if Base10Mantissa=0 then begin
+  Base10Exponent:=0;
+ end;
+
+ if StringPosition>=aStringLength then begin
   result:=ComputeFloat64(Base10Exponent,Base10Mantissa,Negative,aOK);
  end else begin
   result:=UInt64Bits2Double(TPasDblStrUtilsUInt64($7ff8000000000000)); // NaN
+  if assigned(aOK) then begin
+   aOK^:=false;
+  end;
  end;
+
 end;
 
 function EiselLemireStringToDouble(const aStringValue:TPasDblStrUtilsString;const aOK:PPasDblStrUtilsBoolean=nil;const aStrict:boolean=false):TPasDblStrUtilsDouble;
